e94e7be2f394a21983a5f13b7887e7d88f019450
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-12 12:10:24 -0300

    commonjs: modulo.export e require

--- commonjs-modules/src/main.js
+++ commonjs-modules/src/main.js
@@ -1,5 +1,9 @@
+const p = require("./services/products");
+
 async function main(){
-    console.log("Ola");
+    console.log("Carrinho compras:");
+    p.getFullName("408", "mousepad");
+    p.getFullName("508", "mouse");
 }
 
 main()
--- commonjs-modules/src/services/products.js
+++ commonjs-modules/src/services/products.js
@@ -1,5 +1,9 @@
 // todas as funções que lidam com produto
 
 async function getFullName(codeId, productName){
-    return codeId + "--" + productName;
+    console.log("product: " + codeId + "--" + productName);
+}
+
+module.exports = {
+    getFullName,
 }
