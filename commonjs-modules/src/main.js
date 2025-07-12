6db2d1d6d2e3d639bc9b2bc5942b8f0000947444
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-12 13:18:33 -0300

    commonjs: destructuring

--- commonjs-modules/src/main.js
+++ commonjs-modules/src/main.js
@@ -1,15 +1,25 @@
-const product = require("./services/products");
+//destructuring = importar funções específicas somente, importa direto
+const {getFullName, productType} = require("./services/products");
+
+//para importar tudo armazeno na const
+const products = require("./services/products.js");
+
 const config = require("./services/config.js");
 const database = require("./services/database.js");
 
 async function main(){
     console.log("Carrinho compras:");
     
-    // p.getFullName("408", "mousepad");
-    // p.getFullName("508", "mouse");
-    // p.getProductLabel("mousepad");
+    //nao precisa digitar o nome da const, puxa direto
+    getFullName("1", "teclado");
+    console.log(productType);
 
-    database.connectToDatabase("DadosBanco")
+    products.getFullName("408", "mousepad");
+    // products.getFullName("508", "mouse");
+    // products.getProductLabel("mousepad");
+
+
+    database.connectToDatabase("my-data");
 }
 
 main()
