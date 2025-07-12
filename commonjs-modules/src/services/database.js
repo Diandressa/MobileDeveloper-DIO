b5b425bc9f86d3191416322b6e67a81503e9b86b
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-12 13:21:48 -0300

    commonjs: async

--- commonjs-modules/src/services/database.js
+++ commonjs-modules/src/services/database.js
@@ -1,9 +1,11 @@
 //export default = exportar por padrão
 
-exports.connectToDatabase = (dataName) => {
+//export default async
+//async - pedir pro node esperar finalizar a execução da função antes de continuar
+exports.connectToDatabase = async (dataName) => {
     console.log("Se conectando ao banco: " + dataName);
 }
-exports.disconnetDatabase = () => {
+exports.disconnetDatabase = async () => {
     //lógica
     console.log("desconectando");
 }
