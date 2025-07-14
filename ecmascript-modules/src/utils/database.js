cf96aca757f683d6b12b4529dcfc57efd479f171
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-14 12:57:34 -0300

    EcmaScript: export default

---
+++ ecmascript-modules/src/utils/database.js
@@ -0,0 +1,7 @@
+async function connectToDatabase(dataName){
+    //lógica de conexão do banco
+    console.log(`Conectado ao banco ${dataName}`);
+}
+
+//export default - por padrão
+export default connectToDatabase;
