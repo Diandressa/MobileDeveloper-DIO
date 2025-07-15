2399f2b1afafc2bacb43fb0cdc5ab327f1b3de5a
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-15 17:32:01 -0300

    carrinho: calculate total e wishlist part1

--- carrinho-compras/src/index.js
+++ carrinho-compras/src/index.js
@@ -1,11 +1,15 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const cart = [];
+const myCart = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
 const item1 = await createItem("hotheels ferrari", 20.99, 1);
 const item2 = await createItem("hotheels lamborghini", 39.99, 3);
 
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
 //precisa do await para ele terminar de criar antes de poder imprimir
-console.log(item2.subtotal())
+console.log(item2.subtotal());
