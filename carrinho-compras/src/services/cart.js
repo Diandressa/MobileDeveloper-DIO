06dcd757712787b56fbf52af4c066fc708ac4608
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-15 13:53:47 -0300

    carrinho: addItem e calculateTotal

--- carrinho-compras/src/services/cart.js
+++ carrinho-compras/src/services/cart.js
@@ -2,9 +2,9 @@
 
 // Casos de uso --------- Assinaturas dos métodos/funções
 
-//Adicionar um item no carrinho
-async function addItem(userCart){
-    //userCart é o carrinho de um usuário em específico
+//Adicionar um item no carrinho ✅
+async function addItem(userCart, item){
+    userCart.push(item)
 }
 
 //Deletar o item no carrinho
@@ -18,5 +18,15 @@ async function removeItem(userCart, index){
 
 //Calcular total
 async function calculateTotal(userCart){
+    //reduce() permite pegar os items e fazer uma ação com cada um dos itens e reduzir à um valor, armazena na var 'total' e soma o subtotal
+    //totalAcumulador: é o valor que vai acumulando (inicia com 0, o valor inicial que você passou).
+    //item: é cada item do array userCart, um por vez, durante a execução
+    return userCart.reduce((totalAcumulador, item) => totalAcumulador + item.subtotal(), 0);
+}
 
+export {
+    addItem,
+    calculateTotal,
+    removeItem,
+    deleteItem,
 }
