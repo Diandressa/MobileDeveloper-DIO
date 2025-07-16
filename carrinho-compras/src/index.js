cb88b3a950944a7db2e653c409f0bcf1d7beb142
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-15 22:03:15 -0300

    carrinho: calculate total e wishlist part2

--- carrinho-compras/src/index.js
+++ carrinho-compras/src/index.js
@@ -2,14 +2,17 @@ import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
 const myCart = [];
+const myWhishList = [];
 
 console.log("Welcome to the your Shopee Cart!");
 
 const item1 = await createItem("hotheels ferrari", 20.99, 1);
 const item2 = await createItem("hotheels lamborghini", 39.99, 3);
 
-await cartService.addItem(myCart, item1);
-await cartService.addItem(myCart, item2);
-
 //precisa do await para ele terminar de criar antes de poder imprimir
-console.log(item2.subtotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWhishList, item2);
+
+console.log("Shopee Cart TOTAL IS: ");
+await cartService.calculateTotal(myCart);
+await cartService.calculateTotal(myWhishList);
--- carrinho-compras/src/services/cart.js
+++ carrinho-compras/src/services/cart.js
@@ -18,10 +18,8 @@ async function removeItem(userCart, index){
 
 //Calcular total
 async function calculateTotal(userCart){
-    //reduce() permite pegar os items e fazer uma ação com cada um dos itens e reduzir à um valor, armazena na var 'total' e soma o subtotal
-    //totalAcumulador: é o valor que vai acumulando (inicia com 0, o valor inicial que você passou).
-    //item: é cada item do array userCart, um por vez, durante a execução
-    return userCart.reduce((totalAcumulador, item) => totalAcumulador + item.subtotal(), 0);
+    const result = userCart.reduce((totalAcumulador, item) => totalAcumulador + item.subtotal(), 0);
+    console.log(result);
 }
 
 export {
