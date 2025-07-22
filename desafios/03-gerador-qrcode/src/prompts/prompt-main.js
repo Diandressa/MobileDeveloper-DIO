d05bce66c161cf4e5cdcfc0d0ac003210e38bc03
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-21 21:28:00 -0300

    gerador-qrcode: main prompt

--- desafios/03-gerador-qrcode/src/prompts/prompt-main.js
+++ desafios/03-gerador-qrcode/src/prompts/prompt-main.js
@@ -4,6 +4,8 @@ const mainPrompt = [
         description: "Escolha a ferramenta (1 - QRCODE ou 2 - PASSWORD)",
         pattern: /^[1-2]+$/,
         message: "Escolha apenas entre 1 e 2",
-        required: true
+        required: true,
     }
 ]
+
+export default mainPrompt;
