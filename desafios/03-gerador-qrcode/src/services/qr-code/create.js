7992b4715e7d61073ac5c520cd01440cbaec147f
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-07-22 22:33:02 -0300

    gerador-qrcode: qr-code generate

--- desafios/03-gerador-qrcode/src/services/qr-code/create.js
+++ desafios/03-gerador-qrcode/src/services/qr-code/create.js
@@ -1,8 +1,15 @@
 import prompt from "prompt";
-import promptQRCode from "../../prompts/prompt-qrcode";
+import promptQRCode from "../../prompts/prompt-qrcode.js";
+import handle from "./handle.js"
 
 async function createQRCode(){
+    //consumir no get as configurações do prompt-qrcode.js
+    //prompt.get(promptQRCode, (err, result) => {})
 
+    //chamar a função que chama o prompt de perguntas e dentro dele já está chamando a função(pacote) que cria o qrcode, que é o handle
+    prompt.get(promptQRCode, handle);
+
+    prompt.start()
 }
 
 export default createQRCode;
---
+++ desafios/03-gerador-qrcode/src/services/qr-code/handle.js
@@ -0,0 +1,22 @@
+//termo handle -> lidar com / --- ele manipula/lida com uma função específica
+
+//importa o pacote que gera qr-code aqui
+import qrcodeTerminal from "qrcode-terminal";
+import chalk from "chalk";
+
+async function handle (err, result) {
+    //lógica
+    if(err) {
+        console.log("error on application")
+        return;
+    }
+
+    const isSmall = result.type == 2; //retorna true
+    qrcodeTerminal.generate(result.link, {small: isSmall}, (qrcode) => {
+        console.log(chalk.green("QR Code gerado com sucesso:\n"));
+        console.log(qrcode)
+    })
+    
+}
+
+export default handle;
