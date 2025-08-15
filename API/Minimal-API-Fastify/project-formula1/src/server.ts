15062c538a29536fdafabc437a56c57100fcacfa
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-08-15 14:41:43 -0300

    Minimal API Fastify: filter driver

--- API/Minimal-API-Fastify/project-formula1/src/server.ts
+++ API/Minimal-API-Fastify/project-formula1/src/server.ts
@@ -5,13 +5,13 @@ const server = fastify({logger: true});
 const teams = [
     {id: 1, name: "McLaren", base: "Woking, UK"},
     {id: 2, name: "Mercedes", base: "Brackley, UK"},
-    {id: 2, name: "Red Bull Racing", base: "Milton Keynes, UK"},
+    {id: 3, name: "Red Bull Racing", base: "Milton Keynes, UK"},
 ]
 
 const drivers = [
     {id: 1, name: "Mas Verstappen", team: "Red Bull Racing"},
-    {id: 1, name: "Lewis Hamilton", team: "Ferrari"},
-    {id: 1, name: "Lando Norris", team: "McLaren"},
+    {id: 2, name: "Lewis Hamilton", team: "Ferrari"},
+    {id: 3, name: "Lando Norris", team: "McLaren"},
 ]
 
 server.get("/teams", async(request,response) => {
@@ -24,6 +24,24 @@ server.get("/drivers", async(request, response) => {
     response.type("application/json").code(200);
 
     return [drivers]
+})
+
+interface DriverParams{
+    id:string
+}
+
+server.get<{Params:DriverParams}>("/drivers/:id", async (request, response)=>{
+    const id = parseInt(request.params.id);
+    
+    const driver = drivers.find(item => item.id === id);
+
+    if(!driver){
+        response.type("application/json").code(404);
+        return {message: "Driver Not Found"}
+    } else {
+        response.type("application/json").code(200);
+        return {driver};
+    }
 })
 
 server.listen({port: 3333}, ()=> {
