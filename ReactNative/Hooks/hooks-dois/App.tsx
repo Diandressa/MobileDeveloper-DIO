37889f114a2e723635312b3a2fdc749d57cb3286
Andressa Nicolau <andressa_nicolau@hotmail.com>
2025-11-19 13:42:40 -0300

    feat(home): criar página para adicionar e remover itens

--- ReactNative/Hooks/hooks-dois/App.tsx
+++ ReactNative/Hooks/hooks-dois/App.tsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  let quantify:number = 12;
+
+  const removeNumber = () => {
+    quantify = quantify -1
+    console.log(quantify)
+  }
+
+  const addNumber = () => {
+    quantify = quantify +1
+    console.log(quantify)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.buttonRow}>
+        <Button title="-" onPress={removeNumber}/>
+        <Text style={styles.textLabel}>{quantify}</Text>
+        <Button title="+" onPress={addNumber}/>
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +34,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textLabel:{
+    fontSize: 22,
+    marginHorizontal: 15,
+  },
+  buttonRow: {
+    flexDirection: "row",
+  }
 });
