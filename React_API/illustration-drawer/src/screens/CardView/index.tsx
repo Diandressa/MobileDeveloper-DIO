cd9065cad8db144432394a2277addb7f63cb86e4
Andressa Nicolau <andressa_nicolau@hotmail.com>
2026-01-19 17:25:07 -0300

    feat(react_api): create screen's components

---
+++ React_API/illustration-drawer/src/screens/CardView/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, Button, Image} from 'react-native'
+import { styles } from './style';
+
+export default function CardView(){
+    return(
+        <View style={styles.container}>
+            <Text>CardView Works!!</Text>
+        </View>
+    )
+}
